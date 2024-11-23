a8146e56c35d591d71978764874d4c83c8d85a07
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-11-23 22:42:24 +0530

    completed job page structure

--- Frontend/src/components/FilterCard.jsx
+++ Frontend/src/components/FilterCard.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Label } from "@radix-ui/react-label";
+const filterData = [
+  {
+    filterType: "Location",
+    array: ["Delhi", "Banglore", "Gaya", "Patna"],
+  },
+  {
+    filterType: "Industry",
+    array: ["Software", "Pharama", "Garbage", "Food"],
+  },
+  {
+    filterType: "Salary",
+    array: ["10k-12k", "15k-14k", "77k-100k", "150k-200k"],
+  },
+];
 
 const FilterCard = () => {
-  return <div>FilterCard</div>;
+  return (
+    <div className="w-full bg-white p-3 rounded-md">
+      <h1 className="text-lg font-bold">Filter Jobs</h1>
+      <hr className="mt-3" />
+      <RadioGroup>
+        {filterData.map((data, index) => (
+          <div>
+            <h1 className="font-bold text-lg">{data.filterType}</h1>
+            {data.array.map((item, index) => {
+              return (
+                <div className="flex items-center space-x-2 my-2">
+                  <RadioGroupItem value={item} />
+                  <Label>{item}</Label>
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </RadioGroup>
+    </div>
+  );
 };
 
 export default FilterCard;
--- Frontend/src/components/Job.jsx
+++ Frontend/src/components/Job.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { Button } from "./ui/button";
+import { Bookmark } from "lucide-react";
+import { Badge } from "./ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 const Job = () => {
-  return <div>Job</div>;
+  return (
+    <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100">
+      <div className="flex items-center justify-between">
+        <p className="text-sm text-gray-500">2 days ago</p>
+        <Button varient="outline" className="rounded-full" size="icon">
+          <Bookmark />
+        </Button>
+      </div>
+      <div className="flex items-center gap-2 my-2">
+        <Button className="p-6" varient="outline" size="icon">
+          <Avatar>
+            <AvatarImage
+              src="https://picsum.photos/200/300"
+              alt="Company Logo"
+            />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </Button>
+        <div>
+          <h1 className="text-lg font-medium">Company Name</h1>
+          <p className="text-sm font-gray-500">India</p>
+        </div>
+      </div>
+      <div>
+        <h1 className="font-bold text-lg my-2">Title</h1>
+        <p className="text-sm text-gray-600">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi
+          temporibus ea nobis minus odio!
+        </p>
+      </div>
+      <div className="flex items-center gap-2 mt-4">
+        <Badge className="text-blue-700 font-bold">12 position</Badge>
+        <Badge className="text-[#F83002] font-bold">Part Time</Badge>
+        <Badge className="text-[#7290b7] font-bold">12 LPA</Badge>
+      </div>
+      <div className="flex items-center gap-4 mt-4">
+        <Button varient="outline">Details</Button>
+        <Button className="bg-[#c099da]">Save for later</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Job;
